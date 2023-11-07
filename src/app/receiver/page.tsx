40d2a316e27456456d3419326111d5d059d5649a
Jan Kaifer <jan@kaifer.cz>
2023-11-07 22:52:40 +0100

    make receiver accept connections

--- src/app/receiver/page.tsx
+++ src/app/receiver/page.tsx
@@ -1,4 +1,25 @@
+"use client";
+import { useEffect } from "react";
+
+declare var cast: any;
+
 export default function Page() {
+  useEffect(() => {
+    const context = cast.framework.CastReceiverContext.getInstance();
+    const playerManager = context.getPlayerManager();
+
+    const castReceiverOptions = new cast.framework.CastReceiverOptions();
+
+    const playbackConfig = new cast.framework.PlaybackConfig();
+    playbackConfig.autoResumeDuration = 5;
+    castReceiverOptions.playbackConfig = playbackConfig;
+
+    castReceiverOptions.supportedCommands =
+      cast.framework.messages.Command.ALL_BASIC_MEDIA;
+
+    context.start(castReceiverOptions);
+  }, []);
+
   return (
     <audio
       autoPlay
