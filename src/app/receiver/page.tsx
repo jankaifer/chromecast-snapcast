f75b0caa7e6123b449350cdb3275b3c73e7b0d05
Jan Kaifer <jan@kaifer.cz>
2023-11-07 22:58:11 +0100

    added debug logger

--- src/app/receiver/page.tsx
+++ src/app/receiver/page.tsx
@@ -6,7 +6,24 @@ declare var cast: any;
 export default function Page() {
   useEffect(() => {
     const context = cast.framework.CastReceiverContext.getInstance();
-    const playerManager = context.getPlayerManager();
+
+    const castDebugLogger = cast.debug.CastDebugLogger.getInstance();
+    const LOG_RECEIVER_TAG = "Receiver";
+
+    castDebugLogger.loggerLevelByEvents = {
+      "cast.framework.events.category.CORE": cast.framework.LoggerLevel.INFO,
+      "cast.framework.events.EventType.MEDIA_STATUS":
+        cast.framework.LoggerLevel.DEBUG,
+    };
+
+    if (!castDebugLogger.loggerLevelByTags) {
+      castDebugLogger.loggerLevelByTags = {};
+    }
+
+    castDebugLogger.loggerLevelByTags[LOG_RECEIVER_TAG] =
+      cast.framework.LoggerLevel.DEBUG;
+
+    castDebugLogger.info(LOG_RECEIVER_TAG, `Debug logger works !!!`);
 
     const castReceiverOptions = new cast.framework.CastReceiverOptions();
 
