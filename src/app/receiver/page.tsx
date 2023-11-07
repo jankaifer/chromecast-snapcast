0afe2070892777095e803e5f1e429fa6d51f6398
Jan Kaifer <jan@kaifer.cz>
2023-11-07 22:10:13 +0100

    added simple playback

---
+++ src/app/receiver/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <audio
+      autoPlay
+      controls
+      src="https://interactive-examples.mdn.mozilla.net/media/cc0-audio/t-rex-roar.mp3"
+    />
+  );
+}
