6f5b0e2c123afdbaf9b50638ce70611a18868bcc
Jan Kaifer <jan@kaifer.cz>
2023-11-07 22:47:05 +0100

    fix sender app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,30 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
+
+declare var cast: any;
+declare var chrome: any;
+
+let stuffExecuted = false;
 
 export default function Home() {
-  return <p>Hello world</p>;
+  useEffect(() => {
+    if (stuffExecuted) return;
+    stuffExecuted = true;
+    // @ts-ignore
+    window["__onGCastApiAvailable"] = function (isAvailable: boolean) {
+      if (isAvailable) {
+        cast.framework.CastContext.getInstance().setOptions({
+          receiverApplicationId: "1F35E02D",
+          autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
+        });
+      }
+    };
+
+    const script = document.createElement("script");
+    script.src =
+      "//www.gstatic.com/cv/js/sender/v1/cast_sender.js?loadCastFramework=1";
+    document.head.appendChild(script);
+  }, []);
+  return <>try casting</>;
 }
