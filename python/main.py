34057b99c20395e93f2ae98a12187977cbe27b4e
Jan Kaifer <jan@kaifer.cz>
2024-05-14 20:43:02 +0200

    wip

--- python/main.py
+++ python/main.py
@@ -7,6 +7,7 @@ chromecasts, browser = pychromecast.get_chromecasts()
 
 # don't show groups, we wan't to control individual speakers ourself
 chromecasts = list(filter(lambda cc: cc.cast_type != 'group', chromecasts))
+chromecasts.sort(key=lambda cc: cc.name)
 
 print("We have found these:")
 for i, cc in enumerate(chromecasts):
@@ -17,6 +18,10 @@ cast = chromecasts[int(input("Pick one: "))]
 
 print(f'Connecting to {cast.name} ...')
 cast.wait()
+browser.stop_discovery()
+
 print(f'Running snapcast on {cast.name} ...')
 cast.start_app(APP_ID)
+cast.wait()
+print(f'{cast.name} currently has app {cast.app_display_name} with id {cast.app_id}')
 print("Completed.")
