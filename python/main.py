0912535f6c32f553982fb7b76348e46d7b8bc0c0
Jan Kaifer <jan@kaifer.cz>
2024-05-14 21:59:58 +0200

    use receiver even in simple_passive_test

--- python/main.py
+++ python/main.py
@@ -16,8 +16,8 @@ for i, cc in enumerate(chromecasts):
     print(f"{i}. {cc.name}")
 
 print()
-# idx = int(input("Pick one: "))
-idx = 2
+idx = int(input("Pick one: "))
+# idx = 2
 cast = chromecasts[idx]
 
 print(f'Connecting to {cast.name} ...')
