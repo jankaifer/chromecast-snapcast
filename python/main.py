b273a854822489fe3a69e751e908189a21156f83
Jan Kaifer <jan@kaifer.cz>
2024-05-16 21:27:30 +0200

    wip

--- python/main.py
+++ python/main.py
@@ -1,8 +1,8 @@
 import pychromecast
 import time
 
-APP_ID="1F35E02D"
-# APP_ID="CC1AD845"
+MY_APP_ID="1F35E02D"
+GENERIC_APP_ID="CC1AD845"
 
 print("Searching for chromecasts on local network ...")
 chromecasts, browser = pychromecast.get_chromecasts()
@@ -14,23 +14,27 @@ chromecasts.sort(key=lambda cc: cc.name)
 print("We have found these:")
 for i, cc in enumerate(chromecasts):
     print(f"{i}. {cc.name}")
+    cc.wait()
+    cc.start_app(MY_APP_ID)
+    print("app started")
 
-print()
-idx = int(input("Pick one: "))
-# idx = 2
-cast = chromecasts[idx]
+# print()
+# idx = int(input("Pick one: "))
+# # idx = 2
+# cast = chromecasts[idx]
 
-print(f'Connecting to {cast.name} ...')
-cast.wait()
-browser.stop_discovery()
-print("status", cast.status)
+# print(f'Connecting to {cast.name} ...')
+# cast.wait()
+# browser.stop_discovery()
+# print("status", cast.status)
 
-print(f'Running snapcast on {cast.name} ...')
-# cast.start_app(APP_ID)
-cast.start_app(APP_ID)
+# print(f'Running snapcast on {cast.name} ...')
 
-time.sleep(2)
+# # cast.start_app(GENERIC_APP_ID)
+# # time.sleep(2)
+# cast.start_app(MY_APP_ID)
+# time.sleep(2)
 
-print(f'{cast.name} currently has app {cast.app_display_name} with id {cast.app_id}')
-print("status", cast.status)
-print("Completed.")
+# print(f'{cast.name} currently has app {cast.app_display_name} with id {cast.app_id}')
+# print("status", cast.status)
+# print("Completed.")
